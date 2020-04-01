44419888504684f775adde4609d066b907c60331
Aidan Reidel <aidan.reidel@gmail.com>
2020-04-01 12:53:59 -0700

    Removed un-needed heading & prettier

--- src/Components/Awards.js
+++ src/Components/Awards.js
@@ -1,34 +1,34 @@
-import React, { Component } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAward } from "@fortawesome/free-solid-svg-icons";
-
+import React, { Component } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAward } from '@fortawesome/free-solid-svg-icons'
 
 class Awards extends Component {
   constructor(props) {
-    super(props);
+    super(props)
 
-    this.awards = props.awards;
+    this.awards = props.awards
   }
 
   render() {
-    return(
-      <section className="resume-section p-3 p-lg-5 d-flex align-items-center" id="awards">
+    return (
+      <section
+        className="resume-section p-3 p-lg-5 d-flex align-items-center"
+        id="awards"
+      >
         <div className="w-100">
-          <h2 className="mb-5">Awards &amp; Certifications</h2>
+          <h2 className="mb-5">Awards</h2>
           <ul className="fa-ul mb-0">
-          {
-            this.awards.map((data, index) => (
+            {this.awards.map((data, index) => (
               <li key={index}>
                 <FontAwesomeIcon icon={faAward} color="#ffc107" />
                 <span className="ml-2"> {data.awardDetail} </span>
               </li>
-            ))
-          }
+            ))}
           </ul>
         </div>
       </section>
-    );
+    )
   }
 }
 
-export default Awards;
+export default Awards
