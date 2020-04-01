e5d766d7eedeccaef037053f6aec302471ab68e7
Aidan Reidel <aidan.reidel@gmail.com>
2020-04-01 14:06:35 -0700

    Added my analytics id & prettier

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import React, { Component } from 'react';
-import Sidebar from './Components/Sidebar';
-import Landing from './Components/Landing';
-import Experience from './Components/Experience';
-import Education from './Components/Education';
-import Skills from './Components/Skills';
-import Interests from './Components/Interests';
-import Awards from './Components/Awards';
-import profileData from './profileData.json';
-import ReactGA from 'react-ga';
+import React, { Component } from 'react'
+import Sidebar from './Components/Sidebar'
+import Landing from './Components/Landing'
+import Experience from './Components/Experience'
+import Education from './Components/Education'
+import Skills from './Components/Skills'
+import Interests from './Components/Interests'
+import Awards from './Components/Awards'
+import profileData from './profileData.json'
+import ReactGA from 'react-ga'
 
 class App extends Component {
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
-      landingData : profileData.landing,
-      experience : profileData.experience,
-      education : profileData.education,
-      skills : profileData.skills,
-      interests : profileData.interests,
-      awards : profileData.awards
-    };
+      landingData: profileData.landing,
+      experience: profileData.experience,
+      education: profileData.education,
+      skills: profileData.skills,
+      interests: profileData.interests,
+      awards: profileData.awards
+    }
 
-    ReactGA.initialize('UA-158818625-1');
-    ReactGA.pageview(window.location.pathname);
+    ReactGA.initialize('UA-162526072-1')
+    ReactGA.pageview(window.location.pathname + window.location.search)
   }
   render() {
     return (
@@ -34,7 +34,7 @@ class App extends Component {
           <hr className="m-0" />
           <Experience experience={this.state.experience} />
           <hr className="m-0" />
-          <Education education={this.state.education}/>
+          <Education education={this.state.education} />
           <hr className="m-0" />
           <Skills skills={this.state.skills} />
           <hr className="m-0" />
@@ -43,8 +43,8 @@ class App extends Component {
           <Awards awards={this.state.awards} />
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
